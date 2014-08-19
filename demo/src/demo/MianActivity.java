62aa05c25a3cfeb04e525abb3e127145eaa5fc74
wuliao <dazhengxie@gmail.com>
2014-08-19 13:01:50 +0800

    add image

--- demo/src/demo/MianActivity.java
+++ demo/src/demo/MianActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MianActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if(getIntent()!=null){
+			String action=getIntent().getAction();
+			Toast.makeText(getApplicationContext(), action, 0).show();
+		}
 		TextView tv=new TextView(this);
 		tv.setText("demo");
 		setContentView(tv);
